5acbca4af620761c2e95242fa1d743474b9e4982
charles.hourtoule <charles.hourtoule@2548b8ac-2501-017b-5b21-82ff12f84424>
2010-04-28 13:15:03 +0000

    v0.2:       * Added options (regexp modifications, url, etc.).       * Fixed a few bugs.       * Added colors to the output!

--- urlgrep.pl
+++ urlgrep.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #####################################
-# URLgrep v0.1                      #
+# URLgrep v0.2                      #
 # by x0rz <hourto_c@epita.fr        #
 #                                   #
 # http://code.google.com/p/urlgrep/ #
@@ -9,44 +9,106 @@
 
 use LWP::Simple qw($ua get);;
 use HTML::LinkExtor;
+use Term::ANSIColor;
+use Getopt::Long;
+
 use Data::Dumper;
 
 $ua->timeout(5);
 $ua->agent('Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; fr; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3');
 
+# Catching Ctrl-C
+$SIG{INT} = \&tsktsk;
+
+sub tsktsk {
+    print ("\n");
+    print_comm ("Catching Ctrl-C!\n");
+    finishing();
+    exit 0;
+}
+
+
 my @crawled;
 my @targets;
 
-# Options
-$host = "http://www.epita.fr/";
-$depth = 1;
-$regexp = /\?/;
-$verbose = 0;
+my $total_links = 0;
 
-print ("# Running URLgrep on " . $host ."\n");
-print ("# Started on ".gmtime()." \n");
+#
+# 1. Options
+#
+my $host = "http://localhost/";
+my $depth = 1;
+my $regexp = "";
+my $verbose = 0;
+my $help = 0;
 
+GetOptions ('verbose' => \$verbose,
+	    'depth=i' => \$depth,
+	    'url=s' => \$host,
+	    'regexp=s' => \$regexp,
+	    'help' => \$help);
+
+if ($help != 0)
+{
+    print_comm ("URLgrep v0.2\n");
+    print_comm ("by x0rz <hourto_c@epita.fr\n");
+    print_comm ("http://code.google.com/p/urlgrep/\n");
+    print_comm ("\n");
+    print_comm ("usage: ./urlgrep.pl --url=URL [--depth=D --verbose --regexp=REG]\n");
+    exit 0;
+}
+
+if ($host eq "")
+{
+    print_comm ("usage: ./urlgrep.pl --url=URL [--depth=D --verbose --regexp=REG]\n");
+    exit 1;
+}
+
+print_comm ("Running URLgrep on " . $host ."\n");
+print_comm ("Started on ".gmtime()." \n");
+
+#
+# 2. Call first root URL
+#
 parseURL($host, 0);
 
-print ("\n");
-
-print "[*] Crawl done - crawled ".scalar(@crawled)." URL(s).\n";
-
-%seen = ();
-foreach $item (@targets) {
-    push(@targets_u, $item) unless $seen{$item}++;
+if ($verbose == 0)
+{
+    print ("\n");
 }
 
-if (scalar(@targets_u) == 0)
+finishing();
+
+
+sub finishing
 {
-    print "[X] No target found.\n";
-}
-else
-{
-    print "[*] ".scalar(@targets_u)." target(s) found.\n";
-    foreach $link (@targets_u)
+    print_comm ("Finished on ".gmtime()." \n");
+    
+    print_ok();
+    print "Crawl done - crawled ".scalar(@crawled)." URL(s).\n";
+    
+    %seen = ();
+    foreach $item (@targets) {
+	push(@targets_u, $item) unless $seen{$item}++;
+    }
+    
+    if (scalar(@targets_u) == 0)
     {
-	print "- ".$link."\n";
+	print_ko();
+	print color 'red';
+	print "No target found.\n";
+    }
+    else
+    {
+	print_ok();
+
+	print color 'red';
+	print scalar(@targets_u)." URL(s) found matching /".$regexp."/.\n";
+	foreach $link (@targets_u)
+	{
+	    print_info();
+	    print $link."\n";
+	}
     }
 }
 
@@ -54,7 +116,8 @@ sub parseURL
 {
     if ($verbose == 1)
     {
-	print "[*] Trying (d:".$_[1].") " . $_[0] . "\n";
+	print_ok();
+	print "Trying (d:".$_[1].") " . $_[0] . "\n";
     }
     else
     {
@@ -66,7 +129,11 @@ sub parseURL
 
     # Get the HTML page
     my $content = get($_[0]);
-    print "[X] Get failed\n" if (!defined $content);
+    if (!defined $content)
+    {
+	print_ko();
+	print "Could't reach the page.\n";
+    }
 
     # Extract links
     my $parser = HTML::LinkExtor->new();
@@ -83,11 +150,16 @@ sub parseURL
 
     my @links = grep(!/.*\.(gif|jpe?g|png|css|js|ico|swf|axd|jsp|pdf)$/, @links);
 
-    @targets = (@targets, grep($regexp, @links));
+    my @grep = grep(/$regexp/, @links);
+    @targets = (@targets, @grep);
 
     if ($verbose == 1)
     {
-	print "[*] " . scalar(@links) . " links found.\n";
+	print_ok();
+	print  scalar(@links) . " link(s) found.\n";
+	if (scalar(@grep) != 0) {
+	    print "     > " . scalar(@grep)." matched!\n";
+	}
     }
 
     # Testing current depth
@@ -127,6 +199,8 @@ sub constructURL
 	    chop($newURL);
 	}
 
+	
+
 	if ($_[0][0] eq '/')
 	{
 	    $newURL = $newURL . $_[0];
@@ -135,7 +209,52 @@ sub constructURL
 	{
 	    $newURL = $newURL . '/' . $_[0];
 	}
+
+	if (substr($_[0], 1, 2) eq "./")
+	{
+	    
+	}
     }
 
     return ($newURL);
+}
+
+# Misc
+sub print_ok {
+    print color 'bold white';
+    print "[";
+    print color 'green';
+    print "OK";
+    print color 'white';
+    print "] ";
+    print color 'reset';
+}
+
+sub print_ko {
+    print color 'bold white';
+    print "[";
+    print color 'red';
+    print "KO";
+    print color 'white';
+    print "] ";
+    print color 'reset';
+}
+
+sub print_info {
+    print color 'bold white';
+    print "[";
+    print color 'blue';
+    print ">>";
+    print color 'white';
+    print "] ";
+    print color 'reset';
+}
+
+sub print_comm {
+    print color 'bold red';
+    print "# ";
+    print color 'reset';
+    print color 'yellow';
+    print $_[0];
+    print color 'reset';
 }
